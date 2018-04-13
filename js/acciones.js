455af7b6a00d1260a7e6b0e9d866324e700431bf
Josmaymaca <bot.flnetwork@gmail.com>
2018-04-13 07:50:00 -0500

    3Subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,22 +1,22 @@
 // JavaScript Document
 
 $(document).ready(function (e){
-	document.addEventListener ("deviceready",funtion(){
+	document.addEventListener("deviceready",function(){
 		$('disp table td').eq(3).text(device.model);
 		$('disp table td').eq(5).text(device.cordova);
 		$('disp table td').eq(7).text(device.platform);
 		$('disp table td').eq(9).text(device.version);
 		$('disp table td').eq(11).text(device.uuid);
-	document.addEventListener("pause",funtion(){
+	document.addEventListener("pause",function(){
 		escribehistoria ('la app se pausó');
 	},false);//pause
-	document.addEventListener("resume",funtion(){
+	document.addEventListener("resume",function(){
 		escribehistoria ('la app se reinició');
 	},false);//resume
-	document.addEventListener("online",funtion(){
+	document.addEventListener("online",function(){
 		escribehistoria ('la app se conecto a la red');
 	},false);//se conecto
-	document.addEventListener("offline",funtion(){
+	document.addEventListener("offline",function(){
 		escribehistoria ('la app se desconecto de la red');
 	},false);//se desconecto
 
